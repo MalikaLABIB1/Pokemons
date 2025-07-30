7de53554c724ad1c16cd1d02742ad11953411971
MalikaLABIB1 <LABIB.Malika@suptechnology.ma>
2025-07-30 21:12:15 +0100

    Update Pokemon.java

--- Pokemon.java
+++ Pokemon.java
@@ -4,6 +4,7 @@ public class Pokemon {
     private int maxHp;
     private int attack;
     private int movement;
+    private int speed; // Ajoute cette ligne
     private boolean isPlayer2;
 
     public Pokemon(String name, int[] stats, boolean isPlayer2) {
@@ -12,6 +13,7 @@ public class Pokemon {
         this.hp = stats[0];
         this.attack = stats[1];
         this.movement = stats[2];
+        this.speed = stats.length > 3 ? stats[3] : stats[2]; // Ajoute cette ligne
         this.isPlayer2 = isPlayer2;
     }
 
@@ -20,11 +22,29 @@ public class Pokemon {
     public int getMaxHp() { return maxHp; }
     public int getAttack() { return attack; }
     public int getMovement() { return movement; }
+    public int getSpeed() { return speed; } // Ajoute cette méthode
     public boolean isPlayer2() { return isPlayer2; }
 
     public void takeDamage(int damage) {
         hp -= damage;
         if (hp < 0) hp = 0;
+    }
+
+    public void attaque(Pokemon cible) {
+        Pokemon premier, second;
+        if (this.getSpeed() >= cible.getSpeed()) {
+            premier = this;
+            second = cible;
+        } else {
+            premier = cible;
+            second = this;
+        }
+        // Premier attaque second
+        second.takeDamage(premier.getAttack());
+        // Si second est encore vivant, il contre-attaque
+        if (second.getHp() > 0) {
+            premier.takeDamage(second.getAttack());
+        }
     }
 }
 
